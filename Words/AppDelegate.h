6ba7729c5e2d13984ff492c768666074368e6572
Macbook <macbook@macbooks-mbp>
2017-02-08 18:02:21 -0800

    Initial Commit

---
+++ Words/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Words
+//
+//  Created by Macbook on 2/8/17.
+//  Copyright © 2017 Raihana A. Souleymane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Words/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Words
+//
+//  Created by Macbook on 2/8/17.
+//  Copyright © 2017 Raihana A. Souleymane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
